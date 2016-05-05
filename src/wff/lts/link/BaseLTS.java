3a82ff553b3d17353eb7e2fb85058b28f3c2f2b9
fmorbini <fmorbini@gmail.com>
2016-05-05 16:23:18 -0700

    moved the id to the linklts class.

--- src/wff/lts/link/BaseLTS.java
+++ src/wff/lts/link/BaseLTS.java
@@ -4,18 +4,12 @@ import wff.lts.Generator;
 
 public class BaseLTS extends LinkLTS {
 	
-	private static Generator ltsId=new Generator(0);
 	
-	private long id=-1;
 	private String name=null;
 	
 	public BaseLTS(String name) {
 		super(null,null);
-		this.id=ltsId.next();
 		this.name=name;
-	}
-	public long getId() {
-		return id;
 	}
 	
 	@Override
